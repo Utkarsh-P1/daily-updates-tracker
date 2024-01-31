f45286a3f2b83b67ff9f9a853d3b33a7986ee0ba
--global <utkarshpalatkar04@gmail.com>
2024-01-31 15:02:44 +0530

    Resolve build issue

--- src/main/java/com/bytec/tracker/App.java
+++ src/main/java/com/bytec/tracker/App.java
@@ -1,13 +1,17 @@
 package com.bytec.tracker;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Application for managing and tracking daily work in the organisation
  *
  */
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
